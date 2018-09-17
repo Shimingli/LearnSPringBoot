066971d485b57f258389e6fa13d51ac125d1b792
win7 <737141437@qq.com>
2018-09-17 18:54:53 +0800

    1、Configuration 配置的问题 2、数据库的问题

--- springboot-configuration/src/main/java/org/spring/springboot/Application.java
+++ springboot-configuration/src/main/java/org/spring/springboot/Application.java
@@ -15,5 +15,7 @@ public class Application {
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class,args);
+
+
     }
 }
--- springboot-configuration/src/main/java/org/spring/springboot/config/MessageConfiguration.java
+++ springboot-configuration/src/main/java/org/spring/springboot/config/MessageConfiguration.java
@@ -6,9 +6,17 @@ import org.springframework.context.annotation.Configuration;
 /**
  * Created by bysocket on 08/09/2017.
  */
-@Configuration
+@Configuration //Spring Java Configuration这个项目提供了一种通过java代码来装配bean的方案
 public class MessageConfiguration {
+    /*
+     1.使用纯java代码，不在需要xml
 
+     2.在配置中也可享受OO带来的好处
+
+     3.类型安全对重构也能提供良好的支持
+
+     4.依旧能享受到所有springIoC容器提供的功能
+     */
     @Bean
     public String message() {
         return "message configuration";
